b0f49bf48187a2351e209bf28d58a2f736758026
Gerti23 <gertibajo17@gmail.com>
2024-06-18 11:35:24 +0000

    2-print_alphabet.c

---
+++ variables_if_else_while/1-last_digit.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+    last_digit = n %10;
+
+    if(last_digit > 5){
+        printf("Last digit of $d is %d and is less than %d and not 0\n" ,n,last_digit);
+    }
+    if(last_digit == 0){
+        printf("Last digit of %d is %d and is 0\n" ,n,"l");
+    }
+    if(last_digit < 6 && last_digit != 0){
+        printf("Last digit of $d is %d is %d and is less than 6 and not 0\n" ,n,"l");
+    }
+	/* your code goes there */
+	return (0);
+}
---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+char c;
+for(char c = "a"; c <= "z": c ++)
+{
+    putchar(c);
+}
+putchar("\n");
+return(0);
+
+}
