8f509bf9fe989dd60bd34f4d31f0e7197b025a24
Gerti23 <gertibajo17@gmail.com>
2024-06-19 11:52:06 +0000

    0-putchar

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,31 +1,16 @@
-// main.h
-
-#ifndef MAIN_H
-#define MAIN_H
-
-int _putchar(char c);
-
-#endif /* MAIN_H */
-
-// main.c
-
-#include <stdio.h>
 #include "main.h"
 
-int main() {
-    _putchar('_');
-    _putchar('p');
-    _putchar('u');
-    _putchar('t');
-    _putchar('c');
-    _putchar('h');
-    _putchar('a');
-    _putchar('r');
-    _putchar('\n');
-    return 0;
-}
+int main(void)
+{
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
 
-int _putchar(char c) {
-    return putchar(c);
+return (0);
 }
-
