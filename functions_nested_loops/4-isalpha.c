5a80e33a52de9c8b6c685bfdaee39d8112ffdc07
Gerti23 <gertibajo17@gmail.com>
2024-06-20 09:13:53 +0000

    4-isalpha.c

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,13 @@
+		#include "main.h"
+		/**
+		* _islower - checks if a character is lowercase
+		* @c: char passed as argument
+		* Return: 1 if c is lowercase, 0 otherwise
+		*/
+		int_isalpha(int c)
+		{
+		if ((c >= 'a') && (c <= 'z'))  || ((c >= 'A') && (c <= 'Z'))
+		return (1);
+		else
+		return (0);
+		}
