a4a72501c3ebb1ddb8ceb01475db67de819d462f
Gerti23 <gertibajo17@gmail.com>
2024-06-20 09:21:19 +0000

    433

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
-* _isalpha - checks if a character is lowercase or uppercase
-* @c: char passed as argument
-* Return: 1 if c is lowercase or uppercase
-*/
-int_isalpha(int c)
+ * _isalpha - checks if a character is lowercase or uppercase
+ * @c: char passed as argument
+ * Return: 1 if c is lowercase or uppercase
+ */
+int _isalpha(int c)
 {
-	if (c >= 'a' && (c <= 'z'))  || ((c >= 'A' && c <= 'Z'))
-		return (1);
-	else
-		return (0);
+    if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+        return (1);
+    else
+        return (0);
 }
