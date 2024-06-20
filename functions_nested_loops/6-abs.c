a80dc8403deb7e1ba6ee7e17ded384710e14826e
Gerti23 <gertibajo17@gmail.com>
2024-06-20 10:54:06 +0000

    pushh

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *  int _abs - computes the absolute value of an integer.
+ * @n: the number to check
+ *
+ * Return number if number is positive
+ * Return -number if number is negative
+ */
+int _abs(int number)
+{
+	if (n > 0)
+		return ('number');
+	else 
+	return ('-number');
+}	
+
+	
