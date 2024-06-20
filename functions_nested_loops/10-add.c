a57467faafe3266015e2a2d37972bdf3d2691b76
Gerti23 <gertibajo17@gmail.com>
2024-06-20 12:31:50 +0000

    lock

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * 10-add.c - adds two integers and returns the result
+ * @n: the number to extract the last digit from
+ *
+ * Return: the value of the last digit
+ */
+int add(int n)
+{
+	int number;
+	if (int a , int b)
+	return (a + b);
+}
