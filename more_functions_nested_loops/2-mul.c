b4e07e3873b1cba26159cf661d79b5ef6cc043bc
Gerti23 <gertibajo17@gmail.com>
2024-06-24 09:54:48 +0000

    pushh

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isdigit - checks if a character is uppercase.
+ * @c: the character to check
+ *
+ * Return: 1 if c is uppercase, 0 otherwise
+ */
+		int mul(int a, int b)
+		{
+		
+			int result = a * b;
+			return result;
+		}
+
