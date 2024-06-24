641cad0ab30cfa95764123912a52a667062b42d5
Gerti23 <gertibajo17@gmail.com>
2024-06-24 12:04:21 +0000

    update

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * more_numbers - prints numbers from 0 to 14 ten times.
+ */
+void print_line(int n)
+ {
+	int i;
+	for (i = 1; i <= n; i++)
+		_putchar('_');
+	_putchar('\n');
+ }
+ 
