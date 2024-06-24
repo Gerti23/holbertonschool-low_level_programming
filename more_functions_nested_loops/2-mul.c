c74ee0afdeb54e1e36770d1a0f2db749160c2bd7
Gerti23 <gertibajo17@gmail.com>
2024-06-24 10:05:19 +0000

    addd

--- more_functions_nested_loops/2-mul.c
+++ more_functions_nested_loops/2-mul.c
@@ -1,10 +1,10 @@
 #include "main.h"
-	/**
-	* int mul(int a, int b) - Write a function that multiplies two integers .
-	* @c: the character to check
-	*
-	* Return: result if a,b multiplies two integers
-	*/
+/**
+* int mul(int a, int b) - Write a function that multiplies two integers .
+* @c: the character to check
+*
+* Return: result if a,b multiplies two integers
+*/
 		int mul(int a, int b)
 {
 		int result = a * b;
