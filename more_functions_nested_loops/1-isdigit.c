6ef76888cfd2f51bc2f0646072cb1bc2a53d16a5
Gerti23 <gertibajo17@gmail.com>
2024-06-24 09:33:56 +0000

    pushh

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
-* _isupper - checks if a character is uppercase.
-* @c: the character to check
-*
-* Return: 1 if c is uppercase, 0 otherwise
-*/
+ * _isdigit - checks if a character is uppercase.
+ * @c: the character to check
+ *
+ * Return: 1 if c is uppercase, 0 otherwise
+ */
 int _isdigit(int c)
 {
 	if (c >= '0' && c <= '9')
@@ -12,3 +12,4 @@ int _isdigit(int c)
 	else
 		return (0);
 }
+
