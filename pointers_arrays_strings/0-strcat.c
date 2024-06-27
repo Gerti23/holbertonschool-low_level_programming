84f2e258fc3d99e5c92bd86411b237da4f5a8910
Gerti23 <gertibajo17@gmail.com>
2024-06-27 09:07:02 +0000

    first

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -3,21 +3,21 @@
 char *_strcat(char *dest, char *src)
 
 {
-		int i, j;
-		i = 0;
-		while (dest[i] != '\0')
+int i, j;
+i = 0;
+while (dest[i] != '\0')
 {
-		i++;
+i++;
 
 }    
-		j = 0;
-		while (src[j] != '\0')
+j = 0;
+while (src[j] != '\0')
 {
-		dest[i] = src [j];
-		i++;
-		j++;
+dest[i] = src [j];
+i++;
+j++;
 }	
-		dest[i] = '\0';
+dest[i] = '\0';
 
-		return (dest);	
+return (dest);	
 }
