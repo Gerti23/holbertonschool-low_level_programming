2cf0461c828e5d58790ffce84c6a4dbf20eca958
Gerti23 <gertibajo17@gmail.com>
2024-06-27 10:20:24 +0000

    push

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -1,25 +1,30 @@
 #include <stdio.h>
 /**
- * _strncat - concatenates two strings, using at most n bytes from src
+ * _strncpy - Write a function that copies a string.
  * @dest: destination string
  * @src: source string
- * @n: maximum number of bytes to be used from src
+ * @n: Your function should work exactly like strncpy
  *
- * Return: pointer to the resulting string dest
+ * Return: copy
  */
+char *_strncpy(char *dest,  char *src, int n)
+{
+char *copy = dest;
 
-char *_strncpy(char *dest,  char *src, int n) {
-	char *copy = dest;
+	while (*src && n > 0)
 
-	while (*src && n > 0) {
+{
 		*dest++ = *src++;
+		n--;
+
+}
+
+	while (n > 0)
+{
+		*dest++ = '\0';
 		n--;
 }
 
-	while (n > 0) {
-		*dest++ = '\0';
-		n--;
-	}
+	return (copy);
 
-	return copy;
 }
