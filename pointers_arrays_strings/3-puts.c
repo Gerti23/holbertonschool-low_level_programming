135828f8179ee30e7376e83310fc09d00e42af13
Gerti23 <gertibajo17@gmail.com>
2024-06-29 12:53:23 +0000

    update

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -7,10 +7,9 @@
 void _puts(char *str)
 
 {
-	while (*str != '\0')
-	{
-		_putchar (*str);
-		str++;
-	}
-	_putchar ('\n');
+    while (*str) {
+        _putchar(*str);
+        str++;
+    }
+    _putchar('\n');
 }
