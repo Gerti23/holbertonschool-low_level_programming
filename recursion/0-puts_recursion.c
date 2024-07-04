8dc865bd00934d966943420640885d359c8c831f
Gerti23 <gertibajo17@gmail.com>
2024-07-04 09:58:10 +0000

    update

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
-
-void _puts_recursion(char *s) {
+/**
+ * _puts_recursion -entry point,prints string
+ * @s: -holds value for output
+ */
+void _puts_recursion(char *s) 
+{
 if (*s == '\0') {
 	putchar('\n');
 	return;
