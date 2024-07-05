09c835f6bf03bcd3bebaf7973dd2bff104a57a1b
Gerti23 <gertibajo17@gmail.com>
2024-07-05 08:15:05 +0000

    update

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * @argc: argument count
+ * @argv: argument array
+ */
+int main(int argc __attribute__((__unused__)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
