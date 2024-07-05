c2bfb0505c830ff3bb7caf3400db647a1a7afbfa
Gerti23 <gertibajo17@gmail.com>
2024-07-05 08:21:07 +0000

    update

---
+++ argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * @argc: argument count
+ * @argv: argument array
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[] __attribute__((__unused__)))
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
