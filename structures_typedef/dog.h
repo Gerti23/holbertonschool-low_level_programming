0cde80e20ded358a96e6b3ee283926b1081b3dc6
Gerti23 <gertibajo17@gmail.com>
2024-07-11 10:51:42 +0000

    update

---
+++ structures_typedef/dog.h
@@ -0,0 +1,16 @@
+
+
+/**
+ * struct dog - define a dog type
+ * @name: Dog name
+ * @age: Dog age
+ * @owner: Dog owner
+ *
+ * Description: a custom define structure of a dog
+ */
+typedef struct dog 
+{
+    char *name;
+    float age;
+    char *owner;
+} dog;
