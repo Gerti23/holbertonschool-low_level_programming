d222c5404f7f0b724f7d748a8dd8fdc7fc203ac0
Gerti23 <gertibajo17@gmail.com>
2024-07-16 20:58:09 +0000

    changes

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,11 +1,13 @@
 #include "function_pointers.h"
+
 /**
  * print_name - function that prints a name
  * @name: the name
  * @f: function
  */
+
 void print_name(char *name, void (*f)(char *))
 {
-	if (f && name)
-		fname;
+	if (name && f)
+		f(name);
 }
